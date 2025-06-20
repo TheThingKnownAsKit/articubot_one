3c3c20fe1b427b64ae10ece574a4e78b9efc0aa9
TheThingKnownAsKit <moodyellam@gmail.com>
2025-06-20 18:36:30 +0000

    added gazebo imports to docker. put everything into a src folder

---
+++ install/articubot_one/share/articubot_one/launch/sim.launch.py
@@ -0,0 +1,70 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    package_name='articubot_one'
+    
+    # Create a robot_state_publisher node
+    rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name), 'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    # Create the gazebo_sim node
+    gazebo_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')
+        ]),
+        launch_arguments={'gz_args': f'-r {os.path.join(get_package_share_directory(package_name), "worlds", "empty.world")}'}.items() # remove "-r" for paused startup
+    )
+
+    # Spawn the robot into Gazebo
+    spawn_robot = Node(
+        package='ros_gz_sim',
+        executable='create',
+        output='screen',
+        arguments=[
+            '-name', 'rover',
+            '-topic', 'robot_description',
+            '-z', '0.1'
+        ]
+    )
+
+    # Bridge desired ros2 input into Gazebo
+    cmd_vel_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        name='cmd_vel_bridge',
+        output='screen',
+        arguments=[
+            '/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist'
+        ]
+    )
+    lidar2_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        name='lidar2_bridge',
+        output='screen',
+        arguments=[
+            '/lidar2@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan'
+        ]
+    )
+
+    # Launch!
+    return LaunchDescription([
+        rsp,
+        gazebo_sim,
+        spawn_robot,
+        cmd_vel_bridge,
+        lidar2_bridge
+    ])
