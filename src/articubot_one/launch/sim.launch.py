0c8b355d17c576d23123079cf93852873433f450
TheThingKnownAsKit <moodyellam@gmail.com>
2025-06-23 21:26:01 +0000

    tried continual publishing + parameter passing for xml and didnt work

--- src/articubot_one/launch/sim.launch.py
+++ src/articubot_one/launch/sim.launch.py
@@ -3,15 +3,18 @@ from pathlib import Path
 
 from ament_index_python import get_package_share_directory
 import launch
-from launch.actions import IncludeLaunchDescription, ExecuteProcess
+from launch.actions import IncludeLaunchDescription, OpaqueFunction, ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 import xacro
 
 def generate_launch_description():
     package_name='articubot_one'
+    robot_xml = xacro.process_file(
+    os.path.join(get_package_share_directory('articubot_one'),
+                 'description', 'robot.urdf.xacro')).toxml()
 
-    robot_launch_node = IncludeLaunchDescription(
+    robot_description_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
             os.path.join(get_package_share_directory(package_name), 'launch', 'robot.launch.py')
         ]),
@@ -26,7 +29,7 @@ def generate_launch_description():
             os.path.join(get_package_share_directory('ros_gz_sim'), "launch", "gz_sim.launch.py")
         ),
         launch_arguments={
-            'gz_args': f'-v 4 {os.path.join(get_package_share_directory(package_name), 'worlds', 'empty_plane.world')}'
+            'gz_args': f'-v 4 -s libgz_ros_init.so -s libgz_ros_factory.so -s gz-sim-user-commands-system {os.path.join(get_package_share_directory(package_name), 'worlds', 'empty_plane.world')}'
         }.items()
     )
 
@@ -38,7 +41,10 @@ def generate_launch_description():
             '-name', 'articubot_one',
             '-topic', "robot_description",
             '-z' '0.1'
-        ]
+        ],
+        parameters=[{
+            "robot_description": robot_xml
+        }]
     )
 
     # Bridge ROS2 Topics to Gazebo
@@ -62,7 +68,7 @@ def generate_launch_description():
     )
 
     return launch.LaunchDescription([
-        robot_launch_node,
+        robot_description_launch,
         gazebo_launch,
         spawn_entity,
         cmd_vel_bridge,
