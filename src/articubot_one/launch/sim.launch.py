63448c7b227bc68fc201349a9ce68fb90b4981a0
TheThingKnownAsKit <moodyellam@gmail.com>
2025-06-23 21:10:32 +0000

    implemented old multi launch file code. still waits for create to be available

--- src/articubot_one/launch/sim.launch.py
+++ src/articubot_one/launch/sim.launch.py
@@ -6,53 +6,65 @@ import launch
 from launch.actions import IncludeLaunchDescription, ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
+import xacro
 
 def generate_launch_description():
-    # Determine path to the robot URDF file
-    pkg_prefix = get_package_share_directory("articubot_one")
-    ros_gz_prefix = get_package_share_directory("ros_gz_sim")
+    package_name='articubot_one'
 
-    urdf_file  = os.path.join(pkg_prefix, "description", "robot.urdf.xacro")
-    world_file = os.path.join(pkg_prefix, "worlds", "empty_plane.world")
+    robot_launch_node = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(get_package_share_directory(package_name), 'launch', 'robot.launch.py')
+        ]),
+        launch_arguments={
+            'use_sim_time': 'true'
+        }.items()
+    )
 
     # Launch Gazebo Sim (Harmonic)
     gazebo_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(ros_gz_prefix, "launch", "gz_sim.launch.py")
+            os.path.join(get_package_share_directory('ros_gz_sim'), "launch", "gz_sim.launch.py")
         ),
         launch_arguments={
-            'gz_args': f'-v 4 {world_file}'  # Launch empty world (-r for headless if you want no GUI; remove -r for GUI)
+            'gz_args': f'-v 4 {os.path.join(get_package_share_directory(package_name), 'worlds', 'empty_plane.world')}'
         }.items()
     )
 
-    # Spawn the robot into Gazebo using ros_gz_sim create
     spawn_entity = Node(
         package='ros_gz_sim',
         executable='create',
-        arguments=['-name', 'articubot_one', '-world', 'default', '-file', urdf_file],
-        output='screen'
+        output='screen',
+        arguments=[
+            '-name', 'articubot_one',
+            '-topic', "robot_description",
+            '-z' '0.1'
+        ]
     )
 
-    # Bridge /clock topic from Gazebo to ROS 2 (so ROS nodes use sim time)
-    clock_bridge = Node(
+    # Bridge ROS2 Topics to Gazebo
+    cmd_vel_bridge = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
-        arguments=['/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock]'],
-        output='screen'
+        name='cmd_vel_bridge',
+        output='screen',
+        arguments=[
+            '/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist'
+        ]
+    )
+    lidar2_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        name='lidar2_bridge',
+        output='screen',
+        arguments=[
+            '/lidar2@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan'
+        ]
     )
 
-    # (Optional) Additional bridges can be added here for sensors or commands
-    # e.g., bridging TF or joint states if needed:
-    # tf_bridge = Node(
-    #     package='ros_gz_bridge',
-    #     executable='parameter_bridge',
-    #     arguments=['/model/articubot_one/tf@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V'],
-    #     output='screen'
-    # )
-
     return launch.LaunchDescription([
+        robot_launch_node,
         gazebo_launch,
         spawn_entity,
-        clock_bridge,
-        # Include additional bridges or nodes here
+        cmd_vel_bridge,
+        lidar2_bridge
     ])
