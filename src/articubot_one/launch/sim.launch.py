e26580fd609e6b55510aed72a4d08c986da38368
TheThingKnownAsKit <moodyellam@gmail.com>
2025-06-23 21:38:28 +0000

    callable function approach: failed

--- src/articubot_one/launch/sim.launch.py
+++ src/articubot_one/launch/sim.launch.py
@@ -1,4 +1,4 @@
-import os
+import os, xacro, rclpy
 from pathlib import Path
 
 from ament_index_python import get_package_share_directory
@@ -6,71 +6,77 @@ import launch
 from launch.actions import IncludeLaunchDescription, OpaqueFunction, ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
-import xacro
+from std_msgs.msg import String
+from rclpy.qos import QoSProfile, QoSDurabilityPolicy
+
+PKG   = 'articubot_one'
+WORLD = os.path.join(get_package_share_directory(PKG), 'worlds', 'empty_plane.world')
+URDF  = xacro.process_file(
+            os.path.join(get_package_share_directory(PKG),
+                         'description', 'robot.urdf.xacro')).toxml()
+
+def pub_description(context, *args):
+    rclpy.init()
+    node = rclpy.create_node('robot_description_pub')
+    qos = QoSProfile(depth=1,
+                     durability=QoSDurabilityPolicy.TRANSIENT_LOCAL)
+    pub = node.create_publisher(String, 'robot_description', qos)
+    pub.publish(String(data=URDF))
+    node.get_logger().info('Latched robot_description published')
+    rclpy.shutdown()
+
+pub_once = OpaqueFunction(function=pub_description)
+
+# Launch Gazebo Sim (Harmonic)
+gazebo_launch = IncludeLaunchDescription(
+    PythonLaunchDescriptionSource(
+        os.path.join(get_package_share_directory('ros_gz_sim'), "launch", "gz_sim.launch.py")
+    ),
+    launch_arguments={
+        'gz_args': f'-v 4 -s libgz_ros_init.so -s libgz_ros_factory.so -s gz-sim-user-commands-system {WORLD}'
+    }.items()
+)
+
+# Spawn the robot and attach it to the robot_description topic
+spawn_robot = Node(
+    package='ros_gz_sim',
+    executable='create',
+    output='screen',
+    arguments=[
+        '-name', 'articubot_one',
+        '-topic', "robot_description",
+        '-z' '0.1'
+    ],
+    # parameters=[{
+    #     "robot_description": robot_xml
+    # }]
+)
+
+# Bridge ROS2 Topics to Gazebo
+cmd_vel_bridge = Node(
+    package='ros_gz_bridge',
+    executable='parameter_bridge',
+    name='cmd_vel_bridge',
+    output='screen',
+    arguments=[
+        '/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist'
+    ]
+)
+lidar2_bridge = Node(
+    package='ros_gz_bridge',
+    executable='parameter_bridge',
+    name='lidar2_bridge',
+    output='screen',
+    arguments=[
+        '/lidar2@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan'
+    ]
+)
 
 def generate_launch_description():
-    package_name='articubot_one'
-    robot_xml = xacro.process_file(
-    os.path.join(get_package_share_directory('articubot_one'),
-                 'description', 'robot.urdf.xacro')).toxml()
-
-    robot_description_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            os.path.join(get_package_share_directory(package_name), 'launch', 'robot.launch.py')
-        ]),
-        launch_arguments={
-            'use_sim_time': 'true'
-        }.items()
-    )
-
-    # Launch Gazebo Sim (Harmonic)
-    gazebo_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('ros_gz_sim'), "launch", "gz_sim.launch.py")
-        ),
-        launch_arguments={
-            'gz_args': f'-v 4 -s libgz_ros_init.so -s libgz_ros_factory.so -s gz-sim-user-commands-system {os.path.join(get_package_share_directory(package_name), 'worlds', 'empty_plane.world')}'
-        }.items()
-    )
-
-    spawn_entity = Node(
-        package='ros_gz_sim',
-        executable='create',
-        output='screen',
-        arguments=[
-            '-name', 'articubot_one',
-            '-topic', "robot_description",
-            '-z' '0.1'
-        ],
-        parameters=[{
-            "robot_description": robot_xml
-        }]
-    )
-
-    # Bridge ROS2 Topics to Gazebo
-    cmd_vel_bridge = Node(
-        package='ros_gz_bridge',
-        executable='parameter_bridge',
-        name='cmd_vel_bridge',
-        output='screen',
-        arguments=[
-            '/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist'
-        ]
-    )
-    lidar2_bridge = Node(
-        package='ros_gz_bridge',
-        executable='parameter_bridge',
-        name='lidar2_bridge',
-        output='screen',
-        arguments=[
-            '/lidar2@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan'
-        ]
-    )
-
     return launch.LaunchDescription([
-        robot_description_launch,
         gazebo_launch,
-        spawn_entity,
+        pub_once,
+        spawn_robot,
         cmd_vel_bridge,
         lidar2_bridge
     ])
